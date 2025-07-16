f031a45bf0c4defe184450b744a63cc29433544b
Itsuki256 <ktc24a31e0016@edu.kyoto-tech.ac.jp>
2025-07-16 10:49:21 +0900

    create forms

---
+++ crypto/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+
+class EncryptForm(forms.Form):
+    text = forms.CharField(
+        label="暗号化したい文章",
+        widget=forms.Textarea(attrs={
+            'rows': 4,
+            'placeholder': 'ここに暗号化したいテキストを入力してください'
+        })
+    )
+    method = forms.ChoiceField(
+        label="暗号方式",
+        choices=[
+            ('caesar', 'Caesar暗号'),
+            ('base64', 'Base64'),
+        ]
+    )
+
+class DecryptForm(forms.Form):
+    encrypted = forms.CharField(
+        label="復号したい暗号文",
+        widget=forms.Textarea(attrs={
+            'rows': 4,
+            'placeholder': 'ここに復号したい暗号文を入力してください'
+        })
+    )
+    method = forms.ChoiceField(
+        label="暗号方式",
+        choices=[
+            ('caesar', 'Caesar暗号'),
+            ('base64', 'Base64'),
+        ]
+    )
